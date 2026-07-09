3006670c4c60cec10eac0ada3a2ed61ae64192c9
Muhammad Faizan <muhammadfaizan027915@gmail.com>
2026-07-09 20:02:09 +0500

    Refactor backup and certbot scripts to enhance logging and replace mysql-client with mariadb-client

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,23 +1,32 @@
 #!/bin/sh
 set -eu
 
+log() {
+  printf '%s %s\n' "$(date '+%F %T')" "$*"
+}
+
+log "Backup job started"
+
 while true
 do
   DATE=$(date +%F-%H-%M)
   BACKUP_DIR=/backups/$DATE
   ARCHIVE=/backups/backup-$DATE.tar.gz
 
+  log "Starting backup cycle for $DATE"
+  log "Creating backup directory $BACKUP_DIR"
   mkdir -p "$BACKUP_DIR"
 
-  echo 'Backing up PostgreSQL...'
+  log "Backing up PostgreSQL to $BACKUP_DIR/postgres.sql.gz"
   PGPASSWORD="$POSTGRES_PASSWORD" pg_dump \
     -h postgres \
     -U "$POSTGRES_USER" \
     "$POSTGRES_DB" \
     | gzip > "$BACKUP_DIR/postgres.sql.gz"
+  log "PostgreSQL backup completed"
 
-  echo 'Backing up MySQL...'
-  mysqldump \
+  log "Backing up MySQL to $BACKUP_DIR/mysql.sql.gz"
+  mariadb-dump \
     -h mysql \
     -u"$MYSQL_USER" \
     -p"$MYSQL_PASSWORD" \
@@ -25,32 +34,47 @@ do
     --quick \
     --all-databases \
     | gzip > "$BACKUP_DIR/mysql.sql.gz"
+  log "MySQL backup completed"
 
-  echo 'Backing up WordPress files...'
-  tar czf "$BACKUP_DIR/futurestarcarhire.tar.gz" \
+  log "Backing up Future Star Car Hire files"
+  tar --warning=no-file-changed \
+    -czf "$BACKUP_DIR/futurestarcarhire.tar.gz" \
     -C /wordpress futurestarcarhire
+  log "Future Star Car Hire files backup completed"
 
-  tar czf "$BACKUP_DIR/futurestarcarrental.tar.gz" \
+  log "Backing up Future Star Car Rental files"
+  tar --warning=no-file-changed \
+    -czf "$BACKUP_DIR/futurestarcarrental.tar.gz" \
     -C /wordpress futurestarcarrental
+  log "Future Star Car Rental files backup completed"
 
-  echo 'Creating final archive...'
+  log "Creating final archive $ARCHIVE"
   tar czf "$ARCHIVE" -C /backups "$DATE"
+  log "Final archive created"
 
   SIZE=$(du -h "$ARCHIVE" | cut -f1)
+  log "Archive size: $SIZE"
 
-  echo 'Uploading backup...'
+  log "Uploading backup to remote:website-backups/"
   rclone copy "$ARCHIVE" remote:website-backups/
+  log "Upload completed"
 
-  echo 'Sending email notification...'
+  log "Sending email notification to $BACKUP_EMAIL"
   printf '%s\n\n%s\n%s\n%s\n' \
     "Daily backup completed." \
     "File: backup-$DATE.tar.gz" \
     "Size: $SIZE" \
     "Location: remote:website-backups/backup-$DATE.tar.gz" \
     | mutt -s 'Daily Website Backup Completed' -- "$BACKUP_EMAIL"
+  log "Email notification sent"
 
+  log "Removing temporary backup directory $BACKUP_DIR"
   rm -rf "$BACKUP_DIR"
 
+  log "Cleaning up old archives"
   find /backups -name '*.tar.gz' -mtime +7 -delete
+  log "Backup cycle completed for $DATE"
+
+  log "Sleeping for 86400 seconds before next cycle"
   sleep 86400
 done
--- scripts/certbot.sh
+++ scripts/certbot.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 set -eu
 
+log() {
+  printf '%s %s\n' "$(date '+%F %T')" "$*"
+}
+
+log "Certbot job started"
+
+log "Requesting certificate for irfancarsandtours domains"
 certbot certonly \
   --webroot \
   --webroot-path=/var/www/certbot \
@@ -10,7 +17,9 @@ certbot certonly \
   --email "$CERT_EMAIL" \
   -d "$CERT_DOMAIN_IRFAN_CARS_AND_TOURS" \
   -d "$CERT_DOMAIN_IRFAN_CARS_AND_TOURS_WWW"
+log "Certificate request completed for irfancarsandtours"
 
+log "Requesting certificate for futurestarcarhire domains"
 certbot certonly \
   --webroot \
   --webroot-path=/var/www/certbot \
@@ -20,7 +29,9 @@ certbot certonly \
   --email "$CERT_EMAIL" \
   -d "$CERT_DOMAIN_FUTURE_STAR_CAR_HIRE" \
   -d "$CERT_DOMAIN_FUTURE_STAR_CAR_HIRE_WWW"
+log "Certificate request completed for futurestarcarhire"
 
+log "Requesting certificate for futurestarcarrental domains"
 certbot certonly \
   --webroot \
   --webroot-path=/var/www/certbot \
@@ -30,12 +41,16 @@ certbot certonly \
   --email "$CERT_EMAIL" \
   -d "$CERT_DOMAIN_FUTURE_STAR_CAR_RENTAL" \
   -d "$CERT_DOMAIN_FUTURE_STAR_CAR_RENTAL_WWW"
+log "Certificate request completed for futurestarcarrental"
 
 while true
 do
+  log "Starting certificate renewal check"
   certbot renew \
     --webroot \
     --webroot-path=/var/www/certbot
+  log "Certificate renewal check completed"
 
+  log "Sleeping for 12 hours before next renewal check"
   sleep 12h
 done
